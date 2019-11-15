ef27fd6bc9d8da0f3f346c4d993164e7b8315a95
Tony-Moon <anthonyowenmoon@gmail.com>
2019-11-15 12:51:27 -0600

    removed debug prints

--- login/login.go
+++ login/login.go
@@ -22,10 +22,8 @@ const (
 func CheckLogin(log string) (bool, bool) {
 	var logSuccess, authority, au bool
 	logSuccess, authority = false, false
-	fmt.Println("checking login")
 
 	if len(os.Args) == 5 {
-		fmt.Println("correct number of arguments")
 		var job, id, pass, un, pw string
 		job = os.Args[2]
 		id = os.Args[3]
@@ -36,31 +34,25 @@ func CheckLogin(log string) (bool, bool) {
 
 		db := connect()
 		if job == "d" {
-			fmt.Println("checking doctor table")
 			rows, _ := db.Query("SELECT Doc_Username FROM Doctors")
 			for rows.Next() {
 				rows.Scan(&un)
 				if id == un {
-					fmt.Println("found doctor username")
 					row := db.QueryRow("SELECT Doc_Password FROM Doctors WHERE Doc_Username = $1", id)
 					row.Scan(&pw)
 					if pass == pw {
-						fmt.Println("found password")
 						logSuccess = true
 					}
 				}
 			}
 		} else if job == "p" {
-			fmt.Println("checking pharmacist table")
 			rows, _ := db.Query("SELECT Username FROM Pharmacists")
 			for rows.Next() {
 				rows.Scan(&un)
 				if id == un {
-					fmt.Println("found employee id")
 					row := db.QueryRow("SELECT Pharm_Password FROM Pharmacists WHERE Username = $1", id)
 					row.Scan(&pw)
 					if pass == pw {
-						fmt.Println("found password")
 						logSuccess = true
 						aut := db.QueryRow("SELECT Is_Manager FROM Pharmacists WHERE Username = $1", id)
 						aut.Scan(&au)
@@ -84,6 +76,5 @@ func connect() *sql.DB {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("successfully connected to database")
 	return db
 }
