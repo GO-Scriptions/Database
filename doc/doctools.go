fede8a3251576eae0d40779cf3a5ac980659be9d
fjhussai <farwajhussain@gmail.com>
2019-11-15 16:27:05 -0600

    working on view prescriptions

--- doc/doctools.go
+++ doc/doctools.go
@@ -2,6 +2,18 @@ package doc
 
 import (
 	"os"
+	"os/exec"
+	"database/sql"
+	"fmt"
+)
+
+// These constants are the default names for docker containers
+const (
+	host     = "localhost"
+	port     = "5432"
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "postgres"
 )
 
 // ToolNav function for navigating the doctor tools
@@ -29,4 +41,43 @@ func ViewP() string {
 	var r string
 	r = "View Prescriptons"
 	return r
+}
+
+func actuallyVP() {
+
+	if len(os.Args) == 4 {
+		var docun string
+		docun = os.Args[3]
+		
+		//we start up docker
+		dockerstart := exec.Command("docker", "start", "pgcontainer")
+		dockerstart.Run()
+		db := Connect()
+
+		rows, _ := db.Query("SELECT * FROM Prescriptions WHERE Doc_Name = $1", docun)
+		rows.Next() {
+			prescID := make([]string,)
+			var docName string
+			var drugName string
+			var amount int
+			var patientFirst string
+			var patientLast string
+			var cost float64
+			var prescStatus string
+			var datePrescribed string
+		}
+		rows.Scan(&prescID, &docName, &drugName, &amount, &patientFirst, &patientLast)
+	}
+}
+
+// Connect sets up the connection with the database in the docker container
+func Connect() *sql.DB {
+	var conn string
+	conn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
