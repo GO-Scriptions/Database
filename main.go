0bfc1ba5aac919ce8b204ae75703f7b5a469b03c
Tony-Moon <anthonyowenmoon@gmail.com>
2019-11-15 13:02:55 -0600

    add documentation comments

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	log := flag.String("log", "", "Login type. Use the flags d or p login.")
 	flag.Parse()
 
+	// Login flag was passed, see the login folder for more details.
 	if *log != "" {
 		check, aut := login.CheckLogin(*log)
 		if check {
